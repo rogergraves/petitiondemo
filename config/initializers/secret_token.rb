3f7659ee70179e51d38d8fd3d9dc32f00920a873
Roger Graves <rogergraves.gmail.com>
2012-04-04 17:35:54 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petitiondemo::Application.config.secret_token = '26c00200027e909c35a1df423d5b3149e8a2c094362970472464bb4e462c25a4280b3d0e2cab164451ed915fa0be3d9364006b80109964974afd94c761c3b790'
