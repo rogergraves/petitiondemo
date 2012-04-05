06405f36f3c91da14fe43239f8905fb152deb881
Roger Graves <rogergraves.gmail.com>
2012-04-04 19:44:24 -0700

    require user_id and petition_id on signature entries

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,3 +1,5 @@
 class Signature < ActiveRecord::Base
   belongs_to :user
+  validates :user_id, :presence => true
+  validates :petition_id, :presence => true  
 end
--- spec/models/signature_spec.rb
+++ spec/models/signature_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Signature do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @signature = Signature.new
+  end
+  
+  it "must have a user id associated with it" do
+    @signature.should have(1).error_on(:user_id)
+  end
+  
+  it "must have a petition id associated with it" do
+    @signature.should have(1).error_on(:petition_id)
+  end
 end
