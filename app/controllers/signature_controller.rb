87f5da868fd5475f8b535e3bdf4d254e7ec9181e
Roger Graves <rogergraves.gmail.com>
2012-04-04 18:08:04 -0700

    add signature controller

---
+++ app/controllers/signature_controller.rb
@@ -0,0 +1,10 @@
+class SignatureController < ApplicationController
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Petitiondemo::Application.routes.draw do
 
+  get "signature/create"
+  get "signature/edit"
+  get "signature/destroy"
+
   resources :petition
 
   root :to => 'petition#index'
