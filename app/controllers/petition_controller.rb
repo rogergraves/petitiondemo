a4c372f93e3dcf415555d22fc6304f5149a7835b
Roger Graves <rogergraves.gmail.com>
2012-04-04 20:16:22 -0700

    add form for petition

--- app/controllers/petition_controller.rb
+++ app/controllers/petition_controller.rb
@@ -2,9 +2,12 @@ class PetitionController < ApplicationController
   before_filter :authenticate_user!
   
   def new
+    @user_id = params[:id]
+    @petition = Petition.new(user_id: @user_id)
   end
 
   def create
+    @petition = Petition.new(params[:petition])
   end
 
   def update
